6a7d2ea6cc6738c3fcaf8d2b34ec0dc97f5e83f3
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-09-19 21:17:56 +0200

    Add implementation to factory class
    Add implementation to class

--- src/main/java/factory/ExtensionValidatorFactory.java
+++ src/main/java/factory/ExtensionValidatorFactory.java
@@ -1,4 +1,22 @@
 package factory;
 
+import validator.FileExtensionValidator;
+import validator.GifFileExtensionValidator;
+import validator.JpgFileExtensionValidator;
+import validator.TxtFileExtensionValidator;
+
 public class ExtensionValidatorFactory {
+
+    public FileExtensionValidator getValidator(ExtensionType type) {
+        FileExtensionValidator validator = null;
+        switch(type) {
+            case TXT:
+                validator = new TxtFileExtensionValidator();
+            case GIF:
+                validator = new GifFileExtensionValidator();
+            case JPG:
+                validator = new JpgFileExtensionValidator();
+        }
+        return validator;
+    }
 }
