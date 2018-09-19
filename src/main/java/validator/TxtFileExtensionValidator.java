d5a45d5ce782a6667f08284ac121a8b35019af33
augustynSybidlo <augustyn.sybidlo@gmail.com>
2018-09-19 21:16:59 +0200

    Rename txt validator class
    Rename and add implementation to txt validator class

---
+++ src/main/java/validator/TxtFileExtensionValidator.java
@@ -0,0 +1,32 @@
+package validator;
+
+import java.io.*;
+
+public class TxtFileExtensionValidator implements FileExtensionValidator {
+
+    public boolean isValidExtension(File file) {
+        String fileName = file.getName().toUpperCase();
+        boolean extension = fileName.endsWith(".TXT");
+
+        if(!extension) return false;
+        BufferedReader in = null;
+        int sizeOfArray = 1024;
+        char[] chars = new char[sizeOfArray];
+
+        try{
+            in = new BufferedReader(new FileReader(file));
+            in.read(chars);
+            in.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return validate(chars);
+    }
+
+    private boolean validate(char[] chars) {
+        for (char ch : chars) {
+            if (!(ch > 0 && ch < 127)) return false;
+        }
+        return true;
+    }
+}
